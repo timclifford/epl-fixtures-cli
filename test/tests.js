60e4cbe1763ec7ed3c8288cf6f6ad0901ea9aa9a
ecrmnn <danieleckermann@gmail.com>
2016-04-17 01:12:26 +0200

    basic test

--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,11 @@
 'use strict';
 
-const Fixtures = require('epl-fixtures');
 const chai = require('chai');
 const expect = require('chai').expect;
 const exec = require("child_process").exec;
 let captured_stdout = '';
 
 describe('Running CLI without any params', function () {
-  let allMatches = [];
-
-  before(function (done) {
-    new Fixtures().all(function (err, matches) {
-      allMatches = matches;
-      done();
-    });
-  });
 
   before(function (done) {
     exec('node ./src/cli', function (error, stdout, stderr) {
@@ -23,15 +14,7 @@ describe('Running CLI without any params', function () {
     });
   });
 
-  it('returns matches for multiple clubs', function () {
-    const liverpool = captured_stdout.indexOf('Liverpool');
-    const arsenal = captured_stdout.indexOf('Arsenal');
-    const chelsea = captured_stdout.indexOf('Chelsea');
-    const stoke = captured_stdout.indexOf('Stoke');
-
-    expect(liverpool).to.not.equal(-1);
-    expect(arsenal).to.not.equal(-1);
-    expect(chelsea).to.not.equal(-1);
-    expect(stoke).to.not.equal(-1);
+  it('gives output', function () {
+    expect(captured_stdout.length).to.not.equal(0);
   });
 });
