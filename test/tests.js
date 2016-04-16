adb2493963730f32263f7de9c92db47d39db24c0
ecrmnn <danieleckermann@gmail.com>
2016-04-17 00:59:09 +0200

    test adjustments pt .4

--- test/tests.js
+++ test/tests.js
@@ -6,66 +6,71 @@ const expect = require('chai').expect;
 const exec = require("child_process").exec;
 let captured_stdout = '';
 
-describe('Running CLI without any params', function () {
-  let allMatches = [];
+setTimeout(function () {
+  describe('Running CLI without any params', function () {
+    let allMatches = [];
 
-  before(function (done) {
-    new Fixtures().all(function (err, matches) {
-      allMatches = matches;
-      done();
-    });
-  });
-
-  before(function (done) {
-    exec('node ./src/cli', function (error, stdout, stderr) {
-      captured_stdout = stdout;
-      done();
-    });
-  });
-
-  it('returns matches for multiple clubs', function () {
-    console.log(captured_stdout);
-    const liverpool = captured_stdout.indexOf('Liverpool');
-    const arsenal = captured_stdout.indexOf('Arsenal');
-    const chelsea = captured_stdout.indexOf('Chelsea');
-    const stoke = captured_stdout.indexOf('Stoke');
-
-    console.log(liverpool,arsenal,chelsea,stoke);
-
-    expect(liverpool).to.not.equal(-1);
-    expect(arsenal).to.not.equal(-1);
-    expect(chelsea).to.not.equal(-1);
-    expect(stoke).to.not.equal(-1);
-  });
-});
-
-describe('Running CLI with club param', function () {
-  let clubMatches = [];
-
-  before(function (done) {
-    new Fixtures().club('liverpool', function (err, matches) {
-      clubMatches = matches;
-      done();
-    });
-  });
-
-  before(function (done) {
-    exec('node ./src/cli liverpool', function (error, stdout, stderr) {
-      captured_stdout = stdout;
-      done();
-    });
-  });
-
-  it('returns matches for specific clubs', function () {
-    const liverpoolMatches = clubMatches.filter(function (match) {
-      if (match.clubs[0] === 'Liverpool' || match.clubs[1] === 'Liverpool') {
-        return match;
-      }
+    before(function (done) {
+      new Fixtures().all(function (err, matches) {
+        allMatches = matches;
+        done();
+      });
     });
 
-    expect(liverpoolMatches.length).to.be.equal(clubMatches.length);
+    before(function (done) {
+      exec('node ./src/cli', function (error, stdout, stderr) {
+        captured_stdout = stdout;
+        done();
+      });
+    });
+
+    it('returns matches for multiple clubs', function () {
+      const liverpool = captured_stdout.indexOf('Liverpool');
+      const arsenal = captured_stdout.indexOf('Arsenal');
+      const chelsea = captured_stdout.indexOf('Chelsea');
+      const stoke = captured_stdout.indexOf('Stoke');
+
+      console.log(liverpool, arsenal, chelsea, stoke);
+
+      expect(liverpool).to.not.equal(-1);
+      expect(arsenal).to.not.equal(-1);
+      expect(chelsea).to.not.equal(-1);
+      expect(stoke).to.not.equal(-1);
+    });
   });
-});
+  run();
+}, 5000);
+
+setTimeout(function () {
+  describe('Running CLI with club param', function () {
+    let clubMatches = [];
+
+    before(function (done) {
+      new Fixtures().club('liverpool', function (err, matches) {
+        clubMatches = matches;
+        done();
+      });
+    });
+
+    before(function (done) {
+      exec('node ./src/cli liverpool', function (error, stdout, stderr) {
+        captured_stdout = stdout;
+        done();
+      });
+    });
+
+    it('returns matches for specific clubs', function () {
+      const liverpoolMatches = clubMatches.filter(function (match) {
+        if (match.clubs[0] === 'Liverpool' || match.clubs[1] === 'Liverpool') {
+          return match;
+        }
+      });
+
+      expect(liverpoolMatches.length).to.be.equal(clubMatches.length);
+    });
+  });
+  run();
+}, 5000);
 
 describe('Running CLI help command', function () {
   before(function (done) {
